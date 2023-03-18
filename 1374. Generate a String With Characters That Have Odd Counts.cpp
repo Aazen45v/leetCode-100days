5581365599d9f6af7e36999c7a2fb051659820ee
Mohammad Aazen <mohdaazen@gmail.com>
2023-03-19 02:24:27 +0530

    Day 2 of 100 days of coding

---
+++ 1374. Generate a String With Characters That Have Odd Counts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans;
+        if(n%2!=0)
+        {
+         ans=string(n,'x');
+        }
+        else
+        {
+         ans=string(n-1,'x');
+         ans=ans+'b';
+        }
+        return ans;
+    }
+};
