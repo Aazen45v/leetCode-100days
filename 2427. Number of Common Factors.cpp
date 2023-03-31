7c9de30303bbbf9964970969b6aa16d854ae84d2
Mohammad Aazen <mohdaazen@gmail.com>
2023-03-31 19:23:15 +0530

    Day 1 of solving math problem

---
+++ 2427. Number of Common Factors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int i;
+        int ans=0;
+        for(i=1; i<1001; i++){
+            if(a%i==0 && b%i==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
