502312252f568d927c597ef53725d042201ff26a
Mohammad Aazen <mohdaazen@gmail.com>
2024-07-08 12:07:37 +0530

    Update ValidAnagram.cpp
    solved using Hash-Method

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t) {
-            return true;
-        } else {
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if (s == t) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        int freTable[256]={0};
+        for(int i=0;i<s.size();i++){
+            freTable[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            freTable[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+        if(freTable[i]!=0){
             return false;
         }
+        }
+        return true;
     }
 };
